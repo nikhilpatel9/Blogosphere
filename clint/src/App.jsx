1ce112622d84a79eaa786ac622e03e5513144e6d
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-05-18 15:21:37 +0530

    create pages and routes

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -1,6 +1,21 @@
-
+import { BrowserRouter ,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Dashboard from "./pages/Dashboard"
 export default function App() {
   return (
-    <div>App</div>
+  <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/projects" element={<Projects/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+   </Routes>
+  </BrowserRouter>
   )
 }
