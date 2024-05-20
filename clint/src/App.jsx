503a8b7adb70bc9ad87f7c494b5c7c7f562ace8f
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-05-20 17:04:52 +0530

    create the header section

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter ,Routes,Route } from "react-router-dom"
+
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Projects from "./pages/Projects"
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/SignIn"
 import Dashboard from "./pages/Dashboard"
+import Header from "./components/Header"
 export default function App() {
   return (
   <BrowserRouter>
+  <Header/>
    <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/about" element={<About/>}/>
--- clint/tailwind.config.js
+++ clint/tailwind.config.js
@@ -1,11 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { createRequire } from 'module';
+ const require = createRequire(import.meta.url); 
+ const flowbite = require("flowbite-react/tailwind");
+ //import { Button } from "flowbite-react";
+ /** * @type {import('tailwindcss').Config} */
+  export default { 
+    content: [ 
+      "./index.html",
+     "./src/**/*.{js,ts,jsx,tsx}", 
+     "node_modules/flowbite-react/lib/esm/**/*.js,jsx,ts,tsx}", 
+     flowbite.content(),
+    ], 
+     theme: {
+       extend: {}, 
+      }, 
+      plugins:
+       [ 
+        flowbite.plugin(),
+        require('flowbite/plugin'), 
+      ], 
+    };
