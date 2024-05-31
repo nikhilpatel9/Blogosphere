e83a27ade3d7416ba90020b917e4f7391912e2c0
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-05-31 13:56:51 +0530

    complete dark mode functionality

--- clint/src/components/Header.jsx
+++ clint/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link ,useLocation} from "react-router-dom";
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector }from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector,useDispatch }from 'react-redux';
+import { toggleTheme } from "../redux/theme/themeSlice";
 //import { Button } from "flowbite-react";
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch=useDispatch();
+    const {theme} = useSelector((state) => state.theme);
     const {currentUser} = useSelector(state => state.user);
   return (
    <Navbar className='border-b-2'>
@@ -26,8 +29,13 @@ export default function Header() {
          <AiOutlineSearch/>
     </Button>
     <div className='flex gap-3 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color="gray" pill>
-            <FaMoon/> 
+        <Button 
+        className='w-12 h-10 hidden sm:inline' 
+        color="gray" 
+        pill 
+        onClick={()=>dispatch(toggleTheme())}
+        >
+        { theme === 'light' ? <FaSun/> : <FaMoon/>} 
         </Button>
         {currentUser ?(
             <Dropdown
