5dd9f7df52b15a78f23e4c36cec268ca744bb4ca
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-06-14 16:10:35 +0530

    add admin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,7 +20,7 @@ export const signup = async (req, res, next) => {
     password === ''
   ) {
     next(errorHandler(400, 'All fields are required'));
-  }
+  };
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
