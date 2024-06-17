07a231e2d08a4613202f42c44ce2bb6d88833851
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-06-17 08:39:30 +0530

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
  import { verifyToken } from '../utills/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts } from '../controllers/post.controller.js';
 
 const router=express.Router();
 
 router.post('/create',verifyToken,create);
+router.get('/getposts',getposts)
 export default router;
