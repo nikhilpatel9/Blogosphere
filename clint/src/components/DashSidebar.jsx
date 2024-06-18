d36e78f86299f7b0847d8bcce7ab780aaa8d8c01
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-06-18 20:27:35 +0530

    show users to the admin dashboard

--- clint/src/components/DashSidebar.jsx
+++ clint/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight,HiDocumentText } from 'react-icons/hi';
+import { HiUser, HiArrowSmRight,HiDocumentText,HiOutlineUserGroup } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { useEffect,useState } from 'react';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -55,6 +55,17 @@ export default function DashSidebar() {
             icon={HiDocumentText} 
             as='div'>
             Posts 
+            </Sidebar.Item>
+            </Link>
+  
+          )}
+          {currentUser.isAdmin &&(
+            <Link to='/dashboard?tab=users'>
+            <Sidebar.Item 
+            active={tab==='users'} 
+            icon={HiOutlineUserGroup} 
+            as='div'>
+            Users 
             </Sidebar.Item>
             </Link>
   
--- clint/src/pages/Dashboard.jsx
+++ clint/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useState ,useEffect } from 'react'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 export default function Dashboard() {
   const location =useLocation();
   const [tab,setTab] =useState('');
@@ -24,6 +25,8 @@ export default function Dashboard() {
       {tab==='profile' && <DashProfile/>}
       {/* {} */}
       {tab==='posts' &&<DashPosts/>}
+      {/* {} */}
+      {tab==='users'&&<DashUsers/>}
   </div>
     
   )
