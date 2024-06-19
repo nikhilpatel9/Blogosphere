8804336180c4e8b09fd1686eb64513ac2dbcf246
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-06-19 11:58:52 +0530

    complete post page functionality

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./components/PrivateRoute"
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import CreatePost from "./pages/CreatePost"
 import UpdatePost from "./pages/UpdatePost"
+import PostPage from "./pages/PostPage"
 export default function App() {
   return (
   <BrowserRouter>
@@ -29,6 +30,7 @@ export default function App() {
     <Route path="/update-post/:postId" element={<UpdatePost/>}/>
     </Route>
     <Route path="/projects" element={<Projects/>}/>
+    <Route path="/post/:postSlug" element={<PostPage/>}/>
     
    </Routes>
    <Footer></Footer>
