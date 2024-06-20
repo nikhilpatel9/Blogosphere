f4c8d02842503838a269e84e641225d1fbbb8aa0
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-06-20 21:50:02 +0530

    add lke functionality to comment

--- clint/src/components/Comment.jsx
+++ clint/src/components/Comment.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react"
+import { FaThumbsUp } from 'react-icons/fa';
 import moment from 'moment';
-export default function Comment({comment}) {
+import { useSelector } from "react-redux";
+export default function Comment({comment,onLike}) {
     const [user,setUser]=useState({});
-    console.log(user);
+    const {currentUser }= useSelector((state)=>state.user);
     useEffect(() => {
         const getUser = async () => {
           try {
@@ -36,6 +38,22 @@ export default function Comment({comment}) {
             </span>
         </div>
         <p className="text-gray-500 pb-2">{comment.content}</p>
+        <div className='flex items-center pb-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+          <button type="button" 
+          onClick={()=>onLike(comment._id)} 
+          className={`text-gray-400 hover:text-blue-500 
+          ${
+          currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500' 
+          }`}
+          > 
+            <FaThumbsUp className="text-sm"/>
+          </button>
+          <p className="text-gray-400">
+          {
+          comment.numberOfLikes >0 && comment.numberOfLikes +" "+(comment.numberOfLikes ===1 ? "like" : "likes")
+          }
+          </p>
+        </div>
     </div>
     </div>
   );
--- clint/src/components/CommentSection.jsx
+++ clint/src/components/CommentSection.jsx
@@ -3,7 +3,7 @@
 import { Alert, Button, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useSelector} from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link ,useNavigate} from 'react-router-dom';
 import Comment from './Comment';
 
 
@@ -12,6 +12,7 @@ export default function CommentSection({ postId }) {
     const [comment,setComment]=useState('');
     const [commentError,setCommentError]= useState(null);
     const [comments,setComments]=useState([]);
+    const navigate=useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -54,7 +55,31 @@ export default function CommentSection({ postId }) {
     }
     getComments();
   },[postId]) 
-
+ const handleLike =async (commentId)=>{
+  try {
+    if(!currentUser){
+      navigate('/sign-in');
+      return;
+    }
+    const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+      method: 'PUT',
+          });
+          if(res.ok){
+            const data = await res.json();
+            setComments(comments.map((comment) => 
+              comment._id === commentId ? {
+              ...comment,
+              likes: data.likes,
+              numberOfLikes: data.likes.length,
+              }:
+              comment
+              ));
+            }
+          
+  } catch (error) {
+    console.log(error);
+  }
+ }
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         { currentUser ?
@@ -111,7 +136,7 @@ export default function CommentSection({ postId }) {
               <Comment
                 key={comment._id}
                 comment={comment}
-                
+                onLike={handleLike}
               />
             ))}
         </>
