d1f28d16356b90bc337aa27f5d469b3717d8e546
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2024-06-22 12:33:22 +0530

    complelte about and project page

--- clint/src/pages/About.jsx
+++ clint/src/pages/About.jsx
@@ -1,6 +1,34 @@
+/* eslint-disable react/no-unescaped-entities */
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold
+          text-center my-7">About BlogoSphare </h1>
+          <div className="text-md text=gray-500 flex flex-col gap-6">
+            <p>
+              BlogoSphare is a blogging platform that allows users to create, share, and discover content
+              on a wide range of topics. Our mission is to provide a platform where users can express
+              themselves freely, share their ideas, and connect with like-minded individuals.
+            </p>
+            <p>
+              At BlogoSphare, i believe that everyone has a story to tell, and we
+              want to empower users to share their stories with the world. Whether you're a seasoned
+              blogger or just starting out, our platform provides the tools and resources you need to
+              create high-quality content and build a loyal following.
+
+            </p>
+            <p>
+              Our project is dedicated to providing a safe, inclusive, and respectful environment for all users.
+              I believe that everyone deserves to have their voice heard, and we're committed to
+              creating a platform that is free from harassment, bullying, and hate speech.
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- clint/src/pages/Projects.jsx
+++ clint/src/pages/Projects.jsx
@@ -1,6 +1,13 @@
-
+import CallToAction from '../components/CallToAction'
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen mx-w-2xl mx-auto flex justify-center
+    items-center flex-col gap-6 p-3'> 
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Build fun and engaging project while learning
+        HTML,CSS and JavaScript!
+      </p>
+      <CallToAction/>
+    </div>
   )
 }
